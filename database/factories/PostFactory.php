a0a40397e29bdfa024159c815c57fdfc7ad3893d
AHMED-VIR <ahmadasa3447@gmail.com>
2026-03-13 06:29:45 -0500

    Fix: use global \fake() helper for cloud compatibility

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -18,9 +18,9 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => fake()->sentence(),
-            'content' => fake()->paragraphs(3, true),
-            'author' => fake()->name(),
+            'title' => \fake()->sentence(),
+            'content' => \fake()->paragraphs(3, true),
+            'author' => \fake()->name(),
         ];
     }
 }
